38589763513582374065a25f0437354e49f0a219
jader <jaderdaniotti1999@gmail.com>
2025-04-24 12:51:26 +0200

    creazione personal area tocca a te filippo immagini user fratello

--- resources/views/user/personalArea.blade.php
+++ resources/views/user/personalArea.blade.php
@@ -1,1 +1,25 @@
-@dd($user)
+<x-layout>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-lg-4">
+                <section class="card-user">
+                    <div class="card" style="width: 18rem;">
+                        <img src="" class="card-img-top" alt="Immagine utente">
+                        <div class="card-body">
+                          <h5 class="card-title">Nome Utente</h5>
+                          <p class="card-text">Descrizione utente</p>
+                          <p class="card-text">Email: <a href="mailto:utente@example.com">utente@example.com</a></p>
+                          <p class="card-text">Telefono: 1234567890</p>
+                          <button class="btn btn-primary">Visualizza profilo</button>
+                        </div>
+                      </div>
+                </section>
+            </div>
+            <div class="col-12 col-lg-8">
+                <section class="card-user">
+            
+                </section>
+            </div>
+        </div>
+    </div>
+</x-layout>
