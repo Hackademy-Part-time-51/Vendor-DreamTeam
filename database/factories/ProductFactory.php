35ca37d0e9f1b152eebbc66ec88e233e3e8c5028
Mirko Allocca <mirko.allocca1@icloud.com>
2025-04-24 18:51:33 +0200

    Creazione seeder per products

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $categoryIds = Category::pluck('id')->toArray();
+
+         return [
+            'title' => substr(fake()->sentence(3), 0, 20),
+            'price' => fake()->randomFloat(2, 5, 1000),
+
+            'description' => fake()->paragraph(3),
+
+            'category_id' => fake()->randomElement($categoryIds), 
+
+        ];
+    }
+}
