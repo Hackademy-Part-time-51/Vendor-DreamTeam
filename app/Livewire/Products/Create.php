84b191f28191997139d6df92e5eeddf6e90f5773
simone d amico <simodami90@hotmail.it>
2025-04-25 09:13:58 +0200

    sistemato bug creazione prodotto

--- app/Livewire/Products/Create.php
+++ app/Livewire/Products/Create.php
@@ -8,33 +8,36 @@ use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 
 class Create extends Component
-{   public $categories;
-      #[Validate(['required|string|max:50'])]
-    public $name;
-    #[Validate(['required|string|max:2048'])]
+{
+    public $categories;
+    #[Validate('required|string|max:50')]
+    public $title;
+    #[Validate('required|string|max:2048')]
     public $description;
     #[Validate('required|numeric|decimal:0,2|min:0')]
     public $price;
     #[Validate('required|exists:categories,id')]
     public $category_id;
+
     public $user_id;
 
 
 
-    public function create() {
-         $this->validate();
-         $this->user_id = Auth::id();
-         $this->only('title','description','price', 'category_id', 'user_id');
-    
+    public function create()
+    {
+        $this->validate();
+        $this->user_id = Auth::id();
+        //  $this->only('title','description','price', 'category_id', 'user_id');
+
         Product::create(
-            $this->only('title','description','price', 'category_id', 'user_id')
+            $this->only('title', 'description', 'price', 'category_id', 'user_id')
         );
         session()->flash('status', 'Annuncio creato correttamente.');
         return $this->redirect('/products/index');
     }
     public function render()
     {
-        
+
         return view('livewire.products.create');
     }
 }
