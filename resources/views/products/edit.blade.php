ed84a8beaa19df1a19a704554654516d8fe41a30
jader <jaderdaniotti1999@gmail.com>
2025-04-28 21:14:31 +0200

    pagina accesso negato fatta

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     @if($product->user_id !==Auth::id()) 
-       <p>non sei autorizzato </p>
+        <div class="alert alert-danger text-center d-flex justify-content-center align-items-center" style="height: 100vh;">
+            <h1><strong>Accesso Negato!</strong> <br> Non hai i permessi per modificare questo prodotto.</h1>
+        </div>
     @else
     
         @livewire('products.edit', ['product' => $product], ['categories' => $categories])
