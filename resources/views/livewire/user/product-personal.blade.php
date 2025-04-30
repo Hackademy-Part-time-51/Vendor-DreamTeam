505a7607542bdf220b84ba9f40a83d9a263a6f35
simone d amico <simodami90@hotmail.it>
2025-04-30 16:00:01 +0200

    filtro per le card non accettate

--- resources/views/livewire/user/product-personal.blade.php
+++ resources/views/livewire/user/product-personal.blade.php
@@ -1,54 +1,57 @@
 <div class="col-12">
     <div class="row g-3">
         @foreach ($products as $product)
-            <div class="col-12 col-sm-12 col-md-6 col-lg-4 d-flex border-0">
-                <div class="card p-3 ">
+        @if ($product->is_accepted == 1 || Auth::id() == $product->user_id || Auth::user()->is_revisor )
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 d-flex border-0">
+            <div class="card p-3 ">
 
-                    <img src="https://picsum.photos/id/{{ rand(1, 50) }}/1920/1080" class="card-img-top" alt="...">
+                <img src="https://picsum.photos/id/{{ rand(1, 50) }}/1920/1080" class="card-img-top" alt="...">
 
-                    <div class="card-body text-center">
-                        <h5 class="card-title"><a href="{{ route('products.show', $product->id) }}" class="text-decoration-none">{{ Str::limit($product->title, 21) }}</a></h5>
-                        <p class="card-text">{{ Str::limit($product->description, 20) }}</p>
-                    </div>
+                <div class="card-body text-center">
+                    <h5 class="card-title"><a href="{{ route('products.show', $product->id) }}" class="text-decoration-none">{{ Str::limit($product->title, 21) }}</a></h5>
+                    <p class="card-text">{{ Str::limit($product->description, 20) }}</p>
+                </div>
 
-                    @if (Auth::id() == $product->user_id || Auth::user()->is_revisor == 1)
-                        <div class="card-body d-flex justify-content-center gap-1 align-items-center">
-                            <a href="{{ route('products.edit', $product->id) }}" class="btn btn-base">Modifica</a>
-                            <button type="button" data-bs-toggle="modal" data-bs-target="#modalDeleteProduct"
-                                class="btn btn-rosso">
-                                Elimina
-                            </button>
+                @if (Auth::id() == $product->user_id || Auth::user()->is_revisor == 1)
+                    <div class="card-body d-flex justify-content-center gap-1 align-items-center">
+                        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-base">Modifica</a>
+                        <button type="button" data-bs-toggle="modal" data-bs-target="#modalDeleteProduct"
+                            class="btn btn-rosso">
+                            Elimina
+                        </button>
 
-                            <div class="modal fade" id="modalDeleteProduct" data-bs-backdrop="static"
-                                data-bs-keyboard="false" tabindex="-1" aria-labelledby="modalDeleteProductLabel"
-                                aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-centered">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h1 class="modal-title fs-4 text-center" id="modalDeleteProductLabel">
-                                                Vuoi eliminare l'articolo?
-                                            </h1>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-footer justify-content-start">
-                                            <button type="button" class="btn btn-base" data-bs-dismiss="modal">
-                                                Torna indietro
-                                            </button>
-                                            <form action="{{ route('products.destroy', $product->id) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-rosso">Elimina</button>
-                                            </form>
-                                        </div>
+                        <div class="modal fade" id="modalDeleteProduct" data-bs-backdrop="static"
+                            data-bs-keyboard="false" tabindex="-1" aria-labelledby="modalDeleteProductLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-4 text-center" id="modalDeleteProductLabel">
+                                            Vuoi eliminare l'articolo?
+                                        </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-footer justify-content-start">
+                                        <button type="button" class="btn btn-base" data-bs-dismiss="modal">
+                                            Torna indietro
+                                        </button>
+                                        <form action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-rosso">Elimina</button>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    @endif
-                </div>
+                    </div>
+                @endif
             </div>
-        @endforeach
+        </div>
+        @endif
+    @endforeach
+            
     </div>
     {{-- <div class="mt-3">
       {{$products->links()}}
